b7b96c0db039d260c1ae6920955f2a72228f1937
javicond3 <4b3javi@gmail.com>
2020-11-27 18:50:26 +0100

    update draco environment

--- draco/wait_for_draco.sh
+++ draco/wait_for_draco.sh
@@ -1,11 +1,11 @@
-
+DRACO_WEB_PORT=$1
 echo "Waiting for Draco to start processors"
-petition=$(curl -f -s http://draco:9090/nifi-api/system-diagnostics || exit 1) 
+petition=$(curl -f -s http://draco:$DRACO_WEB_PORT/nifi-api/system-diagnostics || exit 1) 
 exit_status=$?
 while [ ${exit_status} -ne 0 ]
 do
     sleep 5
-    petition=$(curl -f -s http://draco:9090/nifi-api/system-diagnostics || exit 1) 
+    petition=$(curl -f -s http://draco:$DRACO_WEB_PORT/nifi-api/system-diagnostics || exit 1) 
     exit_status=$?
 done
 echo "Draco is ready"
